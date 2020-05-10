55a8ac86b11b566923cb6ef6114fbfecd0b0ffb9
yuhur <Yurii.Hurianov@smile-ukraine.com>
2020-05-10 20:49:49 +0300

    refact

--- server.js
+++ server.js
@@ -32,10 +32,8 @@ app.use(passport.initialize());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 
 app.listen(port, () => console.log('is on'));
